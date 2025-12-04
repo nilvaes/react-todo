ceeeb969d0121958711ff84b4013d830f91e5275
Sejus <savas.qrx@gmail.com>
2025-12-04 10:57:21 +0100

    create todo-item component & type, update app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,34 @@
+import { useState } from 'react';
+import TodoItem from './components/TodoItem';
+import { dummyData } from './data/todos';
+
 function App() {
-  return <div className="text-2xl">Hello World!</div>;
+  const [todos, setTodos] = useState(dummyData);
+
+  function setTodoCompleted(id: number, completed: boolean) {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) => (todo.id === id ? { ...todo, completed } : todo))
+    );
+  }
+  return (
+    <main className=" py-10 h-screen space-y-5">
+      <h1 className="font-bold text-3xl text-center">Todo App</h1>
+      <div className="max-w-lg mt-10 text-center mx-auto bg-slate-100 rounded-md p-5">
+        <div className="space-y-1">
+          {/* {dummyData.map((todo) => {
+            return <TodoItem key={todo.id} todo={todo} />;
+          })} */}
+          {todos.map((todo) => (
+            <TodoItem
+              onCompletedChange={setTodoCompleted}
+              key={todo.id}
+              todo={todo}
+            />
+          ))}
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import type { Todo } from '../types/todo';
+
+export const dummyData: Todo[] = [
+  {
+    id: 1,
+    title: 'Do homework',
+    completed: false,
+  },
+  {
+    id: 2,
+    title: 'Do laundries',
+    completed: false,
+  },
+  {
+    id: 3,
+    title: 'Go out and train',
+    completed: false,
+  },
+];
