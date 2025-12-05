1f20420889396c9708663cc0366496b6f13e16c1
Ömer <savas.qrx@gmail.com>
2025-12-05 14:38:26 +0100

    create hook & update and clean App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,17 @@
-import { useState } from 'react';
-import { dummyData } from './data/todos';
 import AddTodoForm from './components/AddTodoForm';
 import TodoList from './components/TodoList';
 import TodoSummary from './components/TodoSummary';
+import useTodos from './hooks/useTodos';
 
 function App() {
-  const [todos, setTodos] = useState(dummyData);
+  const {
+    todos,
+    addTodo,
+    setTodoCompleted,
+    deleteTodo,
+    deleteAllCompletedTodo,
+  } = useTodos();
 
-  function setTodoCompleted(id: number, completed: boolean) {
-    setTodos((prevTodos) =>
-      prevTodos.map((todo) => (todo.id === id ? { ...todo, completed } : todo))
-    );
-  }
-
-  function addTodo(title: string) {
-    setTodos((prevTodos) => [
-      {
-        id: Date.now(),
-        title,
-        completed: false,
-      },
-      ...prevTodos,
-    ]);
-  }
-
-  function deleteTodo(id: number) {
-    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
-  }
-
-  function deleteAllCompletedTodo() {
-    setTodos((prevTodos) => prevTodos.filter((todo) => !todo.completed));
-  }
   return (
     <main className=" py-10 h-screen space-y-5 overflow-y-auto">
       <h1 className="font-bold text-3xl text-center">Todo App</h1>
